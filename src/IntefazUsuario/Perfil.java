98ce57ef39bf8198aa9c8bf7061ce6041ce8a947
Angel <apineiroandion@danielcastelao.org>
2024-04-08 12:04:37 +0200

    añado nombre de usuario en parte superior de la ventana

--- src/IntefazUsuario/Perfil.java
+++ src/IntefazUsuario/Perfil.java
@@ -1,4 +1,32 @@
 package IntefazUsuario;
+import FuncionamientoRed.*;
 
-public class Perfil {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Perfil extends JFrame{
+    public Perfil(Usuarios usuarios, Usuario usuario){
+        setTitle("Perfil Usuario");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        JLabel usernameLabel = new JLabel(usuario.getUserName());
+        usernameLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(usernameLabel, gbc);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
+        add(panel, BorderLayout.NORTH);
+
+        panel.setVisible(true);
+    }
 }
