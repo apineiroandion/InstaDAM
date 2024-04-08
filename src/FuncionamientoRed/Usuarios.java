f7f5e0b28fd9bdec8e3e8be1a42af9129816d889
Angel <apineiroandion@danielcastelao.org>
2024-04-08 11:42:25 +0200

    Creacion de la clase perfil donde cerear la interfaz del perfil

---
+++ src/FuncionamientoRed/Usuarios.java
@@ -0,0 +1,16 @@
+package FuncionamientoRed;
+
+import java.util.ArrayList;
+
+public class Usuarios {
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
+    public Usuarios() {}
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+}
---
+++ src/IntefazUsuario/Perfil.java
@@ -0,0 +1,4 @@
+package IntefazUsuario;
+
+public class Perfil {
+}
