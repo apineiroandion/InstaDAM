8ec3b6bd7c2e522d463ef516d7021b7313a2d7c4
angel <apineiroandion@danielcastelao.org>
2024-04-10 08:45:55 +0200

    añado formato a combobox seguidores y publicaciones, botones y creo clase crear publicacion para recibir los datos

---
+++ src/IntefazUsuario/CrearPublicacion.java
@@ -0,0 +1,4 @@
+package IntefazUsuario;
+
+public class CrearPublicacion {
+}
--- src/IntefazUsuario/Perfil.java
+++ src/IntefazUsuario/Perfil.java
@@ -27,13 +27,60 @@ public class Perfil extends JFrame{
         gbc.gridy = 0;
         panel.add(usernameLabel, gbc);
 
-        DefaultComboBoxModel modelo = new DefaultComboBoxModel(titulosPublicaciones.toArray());
-        DefaultComboBoxModel modelo2 = new DefaultComboBoxModel(nombresUsuariosSeguidos.toArray());
-        JComboBox comboBoxPublicaciones = new JComboBox(modelo);
-        JComboBox comboBoxSiguiendo = new JComboBox(modelo2);
+        //Label Publicaciones
+        JLabel publicacionesLabel = new JLabel("Tus publicaciones");
+        publicacionesLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(publicacionesLabel, gbc);
 
-        panel.add(comboBoxPublicaciones);
-        panel.add(comboBoxSiguiendo);
+        //ComboBox de Publicaciones
+        DefaultComboBoxModel modelo = new DefaultComboBoxModel(titulosPublicaciones.toArray());
+        JComboBox comboBoxPublicaciones = new JComboBox(modelo);
+        comboBoxPublicaciones.setPreferredSize(new Dimension(150, 20));
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        panel.add(comboBoxPublicaciones, gbc);
+
+        //Boton de añadir publicacion
+        JButton nuevaPublicacion = new JButton("Nueva Publicacion");
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        panel.add(nuevaPublicacion, gbc);
+
+        //Label Seguidores
+        JLabel seguidoresLabel = new JLabel("Perfiles que sigues");
+        seguidoresLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        panel.add(seguidoresLabel, gbc);
+
+        //ComboBox de Seguidores
+        DefaultComboBoxModel modelo2 = new DefaultComboBoxModel(nombresUsuariosSeguidos.toArray());
+        JComboBox comboBoxSiguiendo = new JComboBox(modelo2);
+        comboBoxSiguiendo.setPreferredSize(new Dimension(150, 20));
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        panel.add(comboBoxSiguiendo, gbc);
+
+        //Boton para buscar usuarios
+        JButton buscarUsuario = new JButton("Buscar Usuario");
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        panel.add(buscarUsuario, gbc);
+
+
+        nuevaPublicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+
+
+
 
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
         add(panel, BorderLayout.NORTH);
